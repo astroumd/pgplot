73667719ed50468d31b12e9421a5fbb5b224ceb2
Peter Williams <peter@newton.cx>
2007-06-20 21:33:26 +0000

    Add a toplevel autogen.sh and simplify the pgplot one, for portability and probably non-developers will be asked to invoke it.

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,6 @@
 #
 # FIXME: check for correct versions of autotools
 
-echo "HINT: You should only be running this script if you know exactly what "
-echo " it does. If that's not the case, you should download an official release "
-echo " of this package and run its \`configure' script to build the software."
-echo
-
-set -e -x
-
 libtoolize --copy --force
 aclocal
 #autoheader
